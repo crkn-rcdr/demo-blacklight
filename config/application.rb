f4c79cd9ac91f95b58982bf483def3c16a7e9471
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-05-31 16:52:21 -0400

    Adding requested config

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module MyNewBlacklightapp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.web_console.permissions = 'toma.tor.c7a.ca'
+    config.hosts << "toma.tor.c7a.ca"
   end
 end
