68e7effb213d9967256566149deb3392da4bf353
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-05-31 17:01:12 -0400

    Hosts in main file

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module MyNewBlacklightapp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.web_console.permissions = 'toma.tor.c7a.ca'
+    config.hosts << "toma.tor.c7a.ca"
+    config.hosts.clear
   end
 end
