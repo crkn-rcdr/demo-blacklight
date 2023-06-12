65437433eaf59fee96795d9408c10ea9ecafbae5
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-06-12 17:09:38 -0400

    oma ip

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,8 @@ module MyNewBlacklightapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.web_console.permissions = '10.202.1.10'
-    config.hosts << "10.202.1.10"
+    config.web_console.permissions = '10.200.1.35'
+    config.hosts << "10.200.1.35"
     #config.hosts.clear
   end
 end
