1c3a67463e98611ef02e7493c3c6cbf2ab08c033
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-07-25 13:47:25 -0400

    Use internal method

--- app/models/marc_indexer.rb
+++ app/models/marc_indexer.rb
@@ -95,37 +95,9 @@ class MarcIndexer < Blacklight::Marc::Indexer
     # Publication fields
     
     # Remove the accents from the string. Uses String::ACCENTS_MAPPING as the source map.
-    accents_mapping = {
-      'E' => [200,201,202,203],
-      'e' => [232,233,234,235],
-      'A' => [192,193,194,195,196,197],
-      'a' => [224,225,226,227,228,229,230],
-      'C' => [199],
-      'c' => [231],
-      'O' => [210,211,212,213,214,216],
-      'o' => [242,243,244,245,246,248],
-      'I' => [204,205,206,207],
-      'i' => [236,237,238,239],
-      'U' => [217,218,219,220],
-      'u' => [249,250,251,252],
-      'N' => [209],
-      'n' => [241],
-      'Y' => [221],
-      'y' => [253,255],
-      'AE' => [306],
-      'ae' => [346],
-      'OE' => [188],
-      'oe' => [189]
-    }
 
-    remove_accent = lambda  do |rec, acc|
-      acc.map!{|x| accents_mapping.each {|letter,accents|
-          packed = accents.pack('U*')
-          rxp = Regexp.new("[#{packed}]", nil)
-          x.gsub!(rxp, letter)
-        }
-        x 
-      }
+    remove_accent = lambda do |rec, acc|
+      acc.map!{|x| I18n.transliterate(x)}
     end
 
     to_field 'published_ssm', extract_marc('260a', alternate_script: false), remove_accent, trim_punctuation
