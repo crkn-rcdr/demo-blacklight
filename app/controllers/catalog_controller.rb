662909978919b59b72f2efe5ed1b3e83d5b7bca3
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-08-17 13:17:37 -0400

    Collection search results

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -147,6 +147,7 @@ class CatalogController < ApplicationController
     config.add_index_field 'format', label: 'Format'
     config.add_index_field 'author_tsim', label: 'Author'
     config.add_index_field 'language_ssim', label: 'Language'
+    config.add_index_field 'collection_tsim_str', label: 'Collection'
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
