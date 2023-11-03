cb19ddb22ffa3c11cf71d762cf987ba5f6d6bc4c
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-11-03 19:49:58 -0400

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,5 +68,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << 'crkn-blacklight.c7a.ca'
+  config.hosts << 'ec2-54-237-38-135.compute-1.amazonaws.com:3000'
 end
