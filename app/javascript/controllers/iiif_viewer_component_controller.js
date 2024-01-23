4486c61d379e88e0e45687a085bd166b487988a4
Brittny Lapierre <brittny.lapierre@gmail.com>
2024-01-22 20:53:08 -0500

    Better mobile setting

--- app/javascript/controllers/iiif_viewer_component_controller.js
+++ app/javascript/controllers/iiif_viewer_component_controller.js
@@ -746,7 +746,7 @@ export default class extends Controller {
 
         gestureSettingsTouch : {
           scrollToZoom: false,
-          clickToZoom: false
+          dragToPan: false
         }
 
       },
