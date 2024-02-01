a0739bcab654550749dce74d35614318c1df17d9
Brittny Lapierre <brittny.lapierre@gmail.com>
2024-02-01 16:37:35 -0500

    Fix import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,8 +4,10 @@ import "controllers"
 import bootstrap from "bootstrap"
 import githubAutoCompleteElement from "@github/auto-complete-element"
 import Blacklight from "blacklight"
-import * as React from "react";
-//import miradorPdiiifPlugin from "@harvard-lts/mirador-pdiiif-plugin"
+import * as rd from "react-dom";
+import * as rt from "react";
+import * as mv from "mirador";
+import miradorPdiiifPlugin from "@harvard-lts/mirador-pdiiif-plugin"
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
