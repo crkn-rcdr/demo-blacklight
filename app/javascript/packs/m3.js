1bf8295bd5a588e4e5bbb66f62835625097d4af2
Brittny Lapierre <brittny.lapierre@gmail.com>
2024-02-02 19:43:24 -0500

    test

--- app/javascript/packs/m3.js
+++ app/javascript/packs/m3.js
@@ -1,1 +1,9 @@
-import { Mirador, miradorImageToolsPlugin} from "../src/modules/m3_viewer.js";
+import M3Viewer from "../src/modules/m3_viewer.js";
+import { trackView } from "../src/modules/metrics.js";
+
+document.getElementById("sul-embed-object").hidden = false;
+M3Viewer.init();
+
+document.addEventListener("DOMContentLoaded", () => {
+  trackView();
+});
