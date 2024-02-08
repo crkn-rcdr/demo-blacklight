6c0b6df857a231b5d9ceba6e04e8da5e0428d510
Brittny Lapierre <brittny.lapierre@gmail.com>
2024-02-08 12:32:10 -0500

    Add missing reqs

--- app/javascript/src/plugins/miradorPDIIIFDialog.js
+++ app/javascript/src/plugins/miradorPDIIIFDialog.js
@@ -16,8 +16,58 @@ import {
   getCanvases,
   getManifestTitle,
 } from "mirador/dist/es/src/state/selectors";
-import { formatBytes, checkStreamsaverSupport } from "../utils";
 import streamSaver from "streamsaver";
+
+/**
+ * Format bytes to human readable string
+ * https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+ * @param {number} bytes number of bytes
+ * @param {number} decimals number of decimals
+ * @returns {string} human readable string
+ */
+function formatBytes(bytes, decimals = 2) {
+  if (!+bytes) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+}
+
+/**
+ * Check support for streamsaver
+ * Adapted from PDIIIF web example: https://github.com/jbaiter/pdiiif/blob/main/pdiiif-web/src/util.ts
+ */
+function checkStreamsaverSupport() {
+  try {
+    // Our usecase requires WriteableStream
+    new Response(new WritableStream());
+    if (isSecureContext && !("serviceWorker" in navigator)) {
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Check image API has CORS
+ * Adapted from PDIIIF web example: https://github.com/jbaiter/pdiiif/blob/main/pdiiif-web/src/iiif.ts
+ * @returns {boolean} true if image API has CORS
+ */
+async function checkImageApiHasCors() {
+  try {
+    let testImgResp = await fetch(images[0]["@id"] ?? images[0].id);
+    let testImgData = new Uint8Array(await testImgResp.arrayBuffer());
+    return testImgData[0] !== undefined ? true : false;
+  } catch {
+    return false;
+  }
+}
 
 const mapDispatchToProps = (dispatch, { windowId }) => ({
   closeDialog: () => dispatch({ type: "CLOSE_WINDOW_DIALOG", windowId }),
