9d9ce134ff4a2d9e8128c4d554d40de2de33d79e
Brittny Lapierre <brittny.lapierre@gmail.com>
2024-02-08 11:13:21 -0500

    Move code

--- app/javascript/src/modules/m3_viewer.js
+++ app/javascript/src/modules/m3_viewer.js
@@ -2,15 +2,6 @@
 
 import Mirador from 'mirador/dist/es/src/index.js';
 import miradorImageToolsPlugin from 'mirador-image-tools/es/plugins/miradorImageToolsPlugin.js';
-import miradorShareDialogPlugin from 'mirador-share-plugin/es/MiradorShareDialog.js';
-import miradorSharePlugin from 'mirador-share-plugin/es/miradorSharePlugin.js';
-import miradorDownloadPlugin from 'mirador-dl-plugin/es/miradorDownloadPlugin.js';
-import miradorDownloadDialogPlugin from 'mirador-dl-plugin/es/MiradorDownloadDialog.js';
-import shareMenuPlugin from '../plugins/shareMenuPlugin';
-import miradorZoomBugPlugin from '../plugins/miradorZoomBugPlugin';
-import embedModePlugin from '../plugins/embedModePlugin';
-import analyticsPlugin from '../plugins/analyticsPlugin';
-import cdlAuthPlugin from '../plugins/cdlAuthPlugin';
 
 export default {
   init: function() {
