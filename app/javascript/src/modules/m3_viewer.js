34ff6be9afc74b05ff74e29e73a871baa7d81990
Brittny Lapierre <brittny.lapierre@gmail.com>
2024-02-08 11:34:50 -0500

    Add more plugins

--- app/javascript/src/modules/m3_viewer.js
+++ app/javascript/src/modules/m3_viewer.js
@@ -1,7 +1,14 @@
-'use strict';
-
 import Mirador from 'mirador/dist/es/src/index.js';
 import miradorImageToolsPlugin from 'mirador-image-tools/es/plugins/miradorImageToolsPlugin.js';
+import miradorShareDialogPlugin from 'mirador-share-plugin/es/MiradorShareDialog.js';
+import miradorSharePlugin from 'mirador-share-plugin/es/miradorSharePlugin.js';
+import miradorDownloadPlugin from 'mirador-dl-plugin/es/miradorDownloadPlugin.js';
+import miradorDownloadDialogPlugin from 'mirador-dl-plugin/es/MiradorDownloadDialog.js';
+import shareMenuPlugin from '../plugins/shareMenuPlugin';
+import miradorZoomBugPlugin from '../plugins/miradorZoomBugPlugin';
+import embedModePlugin from '../plugins/embedModePlugin';
+//import analyticsPlugin from '../plugins/analyticsPlugin';
+//import cdlAuthPlugin from '../plugins/cdlAuthPlugin';
 
 export default {
   init: function() {
@@ -32,7 +39,14 @@ export default {
         let manifest = "https://www.canadiana.ca/iiif/"+documentId+"/manifest"
         
         let mplugins = [
-          ...miradorImageToolsPlugin
+          ...miradorImageToolsPlugin,
+          ...miradorShareDialogPlugin,
+          ...miradorSharePlugin,
+          ...miradorDownloadPlugin,
+          ...miradorDownloadDialogPlugin,
+          ...shareMenuPlugin,
+          ...miradorZoomBugPlugin,
+          ...embedModePlugin 
         ]
     
         //https://github.com/ProjectMirador/mirador/blob/master/src/config/settings.js
