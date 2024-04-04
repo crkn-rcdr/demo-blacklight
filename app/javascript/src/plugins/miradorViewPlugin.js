59af34034a2e280534fd55117694705f2db1be45
Brittny Lapierre <brittny.lapierre@gmail.com>
2024-04-04 15:16:36 -0400

    Update miradorViewPlugin.js

--- app/javascript/src/plugins/miradorViewPlugin.js
+++ app/javascript/src/plugins/miradorViewPlugin.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import WindowViewSettings from 'mirador/src/containers/WindowViewSettings';
+import WindowViewSettings from 'mirador/dist/es/src/containers/WindowViewSettings';
 
 /**
  */
