63df182e24587020cf2a9c3cd67497cd9a3dd3d5
Brittny Lapierre <brittny.lapierre@gmail.com>
2024-06-25 05:46:48 -0700

    Search form center

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -150,6 +150,8 @@ class CatalogController < ApplicationController
     config.add_index_field 'collection_tsim', label: 'Collection'
     config.add_index_field 'id', label: 'ID'
     config.add_index_field 'is_issue', label: 'Is Issue'
+    config.add_index_field 'pub_date_si', label: 'Date'
+    
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
@@ -239,9 +241,9 @@ class CatalogController < ApplicationController
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case). Add the sort: option to configure a
     # custom Blacklight url parameter value separate from the Solr sort fields.
-    config.add_sort_field 'relevance', sort: 'score desc, pub_date_si desc', label: 'relevance'
+    config.add_sort_field 'relevance', sort: 'score desc', label: 'relevance'
     config.add_sort_field 'year-desc', sort: 'pub_date_si desc', label: 'year'
-    config.add_sort_field 'author', sort: 'author_si asc', label: 'creator'
+    #config.add_sort_field 'author', sort: 'author_si asc', label: 'creator'
     #config.add_sort_field 'title_si asc, pub_date_si desc', label: 'title'
 
     # If there are more than this many search results, no spelling ("did you
